8eea0d5ac09681d3102968beda773fc55eb45d96
Eric <e.khamvongsa@cfa-insta.fr>
2022-02-14 17:12:52 +0100

    V1 page accueil double navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PopularComponent } from './components/popular/popular.component';
+import { CircleImageComponent } from './components/circle-image/circle-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    PopularComponent,
+    CircleImageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    NgbModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/circle-image/circle-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-image',
+  templateUrl: './circle-image.component.html',
+  styleUrls: ['./circle-image.component.css']
+})
+export class CircleImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
